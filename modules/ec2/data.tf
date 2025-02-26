15e8730796aaef3195b75444664ddf2ff7461fdb
Mahmoodi1984 <samadm.008@gmail.com>
2025-02-26 21:40:29 +0100

    ec2 data file for ami id modified

--- modules/ec2/data.tf
+++ modules/ec2/data.tf
@@ -1,18 +1,18 @@
 data "aws_ami" "ubuntu" {
   most_recent = true
 
+  # Filter by image name pattern
   filter {
     name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
   }
 
+  # Filter by virtualization type
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
   }
-}
 
-data "aws_availability_zones" "available" {
-  state = "available"
+  # Filter by the Ubuntu owner ID to ensure the AMI is official
+  owners = ["099720109477"]  # Ubuntu official AWS account ID
 }
-
